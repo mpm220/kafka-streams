6cc634e5e56f30d879db8358648a04ed8bd52403
mpm220 <michaelum215@gmail.com>
2022-06-12 20:43:25 +0100

    First commit
    producer and consumer (App.java) classes

---
+++ src/main/java/com/example/AppConfigs.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class AppConfigs {
+    final static String applicationID = "StreamsApplication";
+    final static String bootstrapServers = "localhost:9092,localhost:29092";
+    
+}
---
+++ src/main/java/com/example/Producer.java
@@ -0,0 +1,33 @@
+package com.example;
+import java.util.Properties;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Producer {
+    private static final Logger logger = LogManager.getLogger();
+    public static void main(String[] args) {
+        logger.info("creating kafka producer");
+        Properties props = new Properties();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfigs.applicationID);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<Integer, String> producer = new KafkaProducer<Integer, String>(props);
+
+        producer.send(new ProducerRecord<>("newTest", 6, "simpleMessage"));
+        producer.close();
+
+    }
+}
