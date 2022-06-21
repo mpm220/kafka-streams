2e10dd934d01ba72affbc060caaed456e238c1f1
mpm220 <michaelum215@gmail.com>
2022-06-21 08:17:39 +0100

    producer changes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,11 +12,6 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
--- src/main/java/com/example/Producer.java
+++ src/main/java/com/example/Producer.java
@@ -2,27 +2,21 @@ package com.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.StreamsConfig;
+import java.util.concurrent.ExecutionException;
 import org.apache.kafka.common.serialization.IntegerSerializer;
-import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.connect.json.JsonSerializer;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.json.JSONArray;
-import org.json.JSONObject;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -35,8 +29,8 @@ public class Producer {
         Properties props = new Properties();
         props.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfigs.applicationID);
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         String sourceAPath = "src\\main\\java\\com\\example\\SourceA.JSON";
         String sourceBPath = "src\\main\\java\\com\\example\\SourceB.JSON";
 
@@ -45,14 +39,26 @@ public class Producer {
         List<Company> sourceB = pullData(sourceBPath);
 
         KafkaProducer<String, Company> producer = new KafkaProducer<String, Company>(props);
-        for(Company company : sourceA){
-            producer.send(new ProducerRecord<>("java-is-pain", company.Ticker, company));
+        //System.out.println(sourceA.get(0).getClass());
+        for(Company company: sourceA){
+            ProducerRecord<String, Company> record = new ProducerRecord<String,Company>("goodFellas", company.getTicker(), company);
+            producer.send(record);
+            String message = "sent message";
+            System.out.println(message);
             producer.close();
+            }
         }
+
+
+        // for(Company company : sourceA){
+        //     producer.send(new ProducerRecord<>("java-is-pain", company.Ticker, company));
+        //     producer.close();
+        // }
         // producer.send(new ProducerRecord<>("newTest", 6, "simpleMessage"));
         // producer.close();
 
-    }
+    
+
     public static List<Company> pullData(String path){
         try (BufferedReader readerA = new BufferedReader(new FileReader(path)))  {
             ObjectMapper mapper = new ObjectMapper();
